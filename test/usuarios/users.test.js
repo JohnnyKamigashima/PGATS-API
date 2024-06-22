6118a1eea8a95d01af29df2fc7dcbcd2c3bbdd21
Johnny Kamigashima <johnny@jhk.app>
2024-06-21 21:05:33 -0300

    valida cadastro e cadastrado

--- test/usuarios/users.test.js
+++ test/usuarios/users.test.js
@@ -4,6 +4,9 @@ const dotEnv = require('dotenv')
 dotEnv.config()
 const baseUrl = process.env.BASE_URL
 const rota = `/${process.env.ROTA_USUARIOS}`
+const faker = require('faker-br')
+const body = new CadastroBody()
+let idRecebido = null
 
 const verificaGeraNovoEmail = async (bodyCriar) => {
     console.log(baseUrl)
@@ -17,24 +20,31 @@ const verificaGeraNovoEmail = async (bodyCriar) => {
     return novoEmail
 }
 describe('Suite de testes da api users...', () => {
+
+    beforeAll(async () => {
+        body.nome = body.fakeNome()
+        body.email = await verificaGeraNovoEmail(body)
+        body.telefone = body.fakeTelefone()
+        body.senha = body.fakeSenha()
+    })
+
     it('Consulta todos os usuários...deve retornar status 200.', async () => {
         const response = await request(baseUrl).get(rota)
 
         expect(response.status).toBe(200)
     })
 
-    it('Deve cadastrar um novo usuário...deve retornar status 200.', async () => {
-        const body = new CadastroBody()
-
-        body.nome = body.fakeNome()
-        body.email = await verificaGeraNovoEmail(body)
-        body.telefone = body.fakeTelefone()
-        body.senha = body.fakeSenha()
+    it('Deve cadastrar um novo usuário...deve retornar status 201.', async () => {
 
         const response = await request(baseUrl).post(rota).send(body)
-        expect(response.status).toBe(200)
+
+        expect(response.body.nome).toBe(body.nome)
+        expect(response.body.email).toBe(body.email)
+        expect(response.body.telefone).toBe(body.telefone)
+        idRecebido = response.body.id
+        expect(response.status).toBe(201)
     })
-    it('Deve cadastrar um novo usuário sem nome...deve retornar status 422.', async () => {
+    it('Deve cadastrar um novo usuário sem nome...deve retornar status 422 e uma mensagem de erro que o campo nome é obrigatório.', async () => {
         const body = new CadastroBody()
         const respostaErro = 'Os seguintes campos são obrigatórios: nome'
 
@@ -46,5 +56,14 @@ describe('Suite de testes da api users...', () => {
         const response = await request(baseUrl).post(rota).send(body)
         expect(response.status).toBe(422)
         expect(response.text).toContain(respostaErro)
+    })
+
+    it('Deve verificar se o usuario foi cadastrado com sucesso...deve retornar status 200.', async () => {
+        const response = await request(baseUrl).get(`${rota}/${idRecebido}`)
+
+        expect(response.status).toBe(200)
+        expect(response.body.email).toBe(body.email)
+        expect(response.body.telefone).toBe(body.telefone)
+        expect(response.body.nome).toBe(body.nome)
     })
 })
